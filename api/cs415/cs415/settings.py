56618f9536cbd38d73d142529bb4c3ae6cf35fdd
Dannymatai <89443475+Dannymatai@users.noreply.github.com>
2024-03-15 01:18:10 -0600

    ..

--- api/cs415/cs415/settings.py
+++ api/cs415/cs415/settings.py
@@ -34,9 +34,9 @@ CORS_ORIGIN_WHITELIST = (
     "http://127.0.0.1:8000",
     "http://localhost:3000",
     "http://54.157.136.87",
-    "http://54.157.136.87"
     
 )
+
 # Application definition
 
 INSTALLED_APPS = [
