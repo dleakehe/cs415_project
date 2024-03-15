b6cafe028e78dbcc9cbd636a42a9e4f0c76ca3df
Dannymatai <89443475+Dannymatai@users.noreply.github.com>
2024-03-15 00:30:24 -0600

    added whitelists

--- api/cs415/cs415/settings.py
+++ api/cs415/cs415/settings.py
@@ -33,6 +33,8 @@ CORS_ORIGIN_WHITELIST = (
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://localhost:3000",
+    "http://54.157.136.87",
+    "http://54.157.136.87"
     
 )
 # Application definition
